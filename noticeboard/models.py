6a7dd2e5f9f1f2f60f158b95727d2d4d399bb2d9
Diganta Biswas <digantaprotik@gmail.com>
2019-02-08 14:03:34 +0600

    bengali url and slug

--- noticeboard/models.py
+++ noticeboard/models.py
@@ -3,6 +3,8 @@ from django.db.models.signals import pre_save
 from django.utils.text import slugify 
 from .utils import get_unique_slug
 
+import random 
+
 # from progotischool.utils import unique_slug_generator
 # Create your models here.
 class Notice(models.Model):
@@ -12,7 +14,7 @@ class Notice(models.Model):
 	picture = models.ImageField(upload_to='images/',null = True, blank=True)
 	upload_date = models.DateTimeField(auto_now_add=True)
 	valid_till = models.DateTimeField()
-	slug = models.SlugField(max_length=40,unique=True,blank=True,allow_unicode=True)
+	slug = models.SlugField(max_length=40,unique=True,blank=True)
 
 	def __str__(self):
 		return self.title
@@ -25,16 +27,17 @@ class Notice(models.Model):
 	# 	    unique_slug = '{}-{}'.format(slug, num)
 	# 	    num += 1
 	# 	return unique_slug
- 
-	# def save(self, *args, **kwargs):
-	#     if not self.slug:
-	#         self.slug = self._get_unique_slug()
-	#     super().save(*args, **kwargs)
 
-	def save(self, *args, **kwargs):
-		if not self.slug:
-		    self.slug = get_unique_slug(self, 'title', 'slug')
-		super().save(*args, **kwargs)
+
+
+	# def save(self, *args, **kwargs):
+	# 	if not self.slug:
+	# 	    self.slug = get_unique_slug(self, 'title', 'slug')
+	# 	super().save(*args, **kwargs)
+
+
+
+
 
 # def post_pre_save_receiver(instance,sender,*args,**kwargs):
  
@@ -44,3 +47,10 @@ class Notice(models.Model):
  
  
 # pre_save.connect(post_pre_save_receiver,sender=Notice)
+
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = get_unique_slug(self,self.title, 'slug')
+		super(Notice, self).save(*args, **kwargs)
+
--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # url(r'^(?P<slug>[-\w\d]+),(?P<id>\d+)/$',views.notice_detail, name ='notice_detail')
  
     path('noticeboard',views.notice_list, name = 'notice_list_url'),
-    path('noticeboard/<slug:slug>',views.notice_detail, name='notice_detail_url'),
+    path('noticeboard/<str:slug>',views.notice_detail, name='notice_detail_url'),
     path('noticeboard/download/<path:path>',views.download_view, name = 'download_url'),
 ]
