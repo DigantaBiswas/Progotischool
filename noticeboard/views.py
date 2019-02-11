2c559a7f6092a29dbeb9e86ed0e4cc40fb56c559
Diganta Biswas <digantaprotik@gmail.com>
2019-02-11 15:21:34 +0600

    fixed footer, pagination

--- noticeboard/views.py
+++ noticeboard/views.py
@@ -1,15 +1,20 @@
 import os
 from django.conf import settings
 from django.http import HttpResponse
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render,redirect,get_object_or_404
 from django.utils import timezone 
+
 from .models import Notice
 # Create your views here.
 def notice_list(request):
-	notices = Notice.objects.all().order_by('-upload_date')
+    notices = Notice.objects.all().order_by('-upload_date')
 
-	return render(request, 'noticeboard/noticeboard.html', {'notices':notices})
+    paginator = Paginator(notices, 12)
+    page = request.GET.get('page')
+    notices = paginator.get_page(page)
+
+    return render(request, 'noticeboard/noticeboard.html',{'notices':notices})
 
 def notice_detail(request, slug):
 	notice = get_object_or_404(Notice, slug=slug)
